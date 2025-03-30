9bde6fb64ba3e9649f0ed3f9302851db29702044
RConradGe <rcgadduang@gmail.com>
2025-03-30 10:03:51 -0700

    wip: implemented search functionality

--- src/StarshipSearch/StarshipSearch.jsx
+++ src/StarshipSearch/StarshipSearch.jsx
@@ -1,3 +1,33 @@
-const StarshipSearch = () => {};
+import { useState } from 'react';
+
+const StarshipSearch = (props) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [prevSearchTerm, setPrevSearchTerm] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.search(searchTerm)
+    setPrevSearchTerm(searchTerm);
+    setSearchTerm('');
+  }
+
+  return (
+    <section>
+      <h2>Search</h2>
+      <form onSubmit={handleSubmit}>
+        <label htnmlfor='searchTerm'>Search Term:</label>
+        <input
+          type='text'
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+    </section>
+  );
+
+
+
+};
 
 export default StarshipSearch;
