072654909e66c151c909498d22f4fe9031f4cd2b
RConradGe <rcgadduang@gmail.com>
2025-03-30 08:09:31 -0700

    wip: passed starshipsData to StarshipCard

--- src/StarshipList/StarshipList.jsx
+++ src/StarshipList/StarshipList.jsx
@@ -1,3 +1,18 @@
-const StarshipList = () => {};
+import StarshipCard from '../StarshipCard/StarshipCard';
+
+const StarshipList = ({ starshipsData }) => {
+
+  // const starshipsList = starshipsData.map((starship, index) =>
+  //   <StarshipCard starship={starship} index={index} />
+  // )
+  return (
+    <div className='starshipList'>
+      <h2>Starships</h2>
+      <ul>
+        <StarshipCard starshipsData={starshipsData} />
+      </ul>
+    </div>
+  );
+};
 
 export default StarshipList;
