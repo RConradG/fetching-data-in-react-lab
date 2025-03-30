2a28e2477071641d3f0c62ef902e29f869f4fcf9
RConradGe <rcgadduang@gmail.com>
2025-03-30 08:08:48 -0700

    Added starship card functionality to display starship info

--- src/StarshipCard/StarshipCard.jsx
+++ src/StarshipCard/StarshipCard.jsx
@@ -1,3 +1,16 @@
-const StarshipCard = () => {};
+const StarshipCard = ({ starshipsData }) => {
+  return (
+    <>
+      {starshipsData.map((starship, index) => (
+        <li key={index} className='card'>
+          <h3>{starship.name}</h3>
+          <p>Class: {starship.starship_class}</p>
+          <p>Manufacturer: {starship.manufacturer}</p>
+          <p>Model: {starship.model}</p>
+        </li>
+      ))}
+    </>
+  );
+};
 
 export default StarshipCard;
