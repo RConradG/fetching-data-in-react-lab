d4d1b81c057b10337cf370c9570ca236c2dadec0
RConradGe <rcgadduang@gmail.com>
2025-03-30 11:18:09 -0700

    wip: added search and clear button functionality

--- src/StarshipSearch/StarshipSearch.jsx
+++ src/StarshipSearch/StarshipSearch.jsx
@@ -11,9 +11,23 @@ const StarshipSearch = (props) => {
     setSearchTerm('');
   }
 
+  const handleClearButton = (event) => {
+      event.preventDefault();
+      props.search('');
+  };
+
+  const searchQueryText = () => {
+    if (!prevSearchTerm) {
+      return 'Search for a starship by name';
+    }
+    return prevSearchTerm;
+  }
+
   return (
     <section>
-      <h2>Search</h2>
+      <h2>Number of Starships Displayed: {props.numberOfStarships}</h2>
+      <h2>Previous Search Query: {searchQueryText()}</h2>
+      <h2>Search:</h2>
       <form onSubmit={handleSubmit}>
         <label htnmlfor='searchTerm'>Search Term:</label>
         <input
@@ -23,11 +37,15 @@ const StarshipSearch = (props) => {
         />
         <button type='submit'>Submit</button>
       </form>
+        <div>
+          {!prevSearchTerm ? null : 
+          <button onClick={handleClearButton}>
+            Show All Starships
+          </button>
+          }
+        </div>
     </section>
   );
-
-
-
 };
 
 export default StarshipSearch;
