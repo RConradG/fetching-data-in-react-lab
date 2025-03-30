7f1a3b973aaca2b5ad1ddfacb8a429186f54cce1
RConradGe <rcgadduang@gmail.com>
2025-03-29 23:37:37 -0700

    wip: added useEffect to update StarshipsData nd setDisplayedStarships

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import StarshipList from './StarshipList/StarshipList';
 import StarshipSearch from './StarshipSearch/StarshipSearch';
+import * as starshipService from './services/starshipService';
 
 import './App.css';
 
@@ -12,8 +12,30 @@ const App = () => {
   const [starshipsData, setStarshipsData] = useState([]);
   const [displayedStarships, setDisplayedStarships] = useState([]);
 
+  useEffect( () => {
+    const fetchDefaultData = async () => {
+      const data = await starshipService.showStarshipList();
+      const newStarshipsData = data.map(starship => ({
+        name: starship.name,
+        starshipClass: starship.starship_class,
+        manufacturer: starship.manufacturer,
+        model: starship.model,
+      }));
+      setStarshipsData(newStarshipsData);
+      setDisplayedStarships(newStarshipsData);
+      console.log(newStarshipsData);
+      console.log(displayedStarships);
+      };
+      
+      fetchDefaultData();
+      
+    }, []);
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+      <button>Fetch</button>
+    </>
   );
 }
 
